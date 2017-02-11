6cbd1d1ef816856081a6a34d6ee8e5f83ce8dbd0
Bartosz Bajorek <bartosz.bajorek@gmail.com>
2017-02-11 18:04:52 +0100

    - lets improve

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rPiphun
 simple tools and toys for rpi
+
+Needed libraries:
+- Adafruit_DHT (https://github.com/adafruit/Adafruit_Python_DHT)
+
---
+++ simple-pir.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)         #Read output from PIR motion sensor
+while True:
+       i=GPIO.input(17)
+       if i==0:                 #When output from motion sensor is LOW
+             print "Gitareczka."
+             time.sleep(0.5)
+       elif i==1:               #When output from motion sensor is HIGH
+             print "Intruz kurwa!",i
+             time.sleep(0.5)
